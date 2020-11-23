c885084246e94bba33406500209b3ff04964ae55
Daniil Viliavin <89505105005@mail.ru>
2020-11-23 21:02:59 +0300

    draft

--- src/controllers/questionResultsController.js
+++ src/controllers/questionResultsController.js
@@ -34,8 +34,8 @@ exports.addQuestionResults = async (req, reply) => {
         let existQuestion = await QuestionResults.findOne({
             theme_id: theme_id,
             user_id: user_id,
-            test_id: new mongoose.Types.ObjectId(test_id),
-            '_id': new mongoose.Types.ObjectId(question)
+            test_id: test_id,
+            question: question
         })
         let currentQuestionResult;
         if (existQuestion == null) {
@@ -61,8 +61,8 @@ exports.addQuestionResults = async (req, reply) => {
         if (currentQuestionResult == null)
             pointPerResult = 0
         else {
-            if (question.right_answers.every(v => currentQuestionResult.user_answers.indexOf(v) >= 0)) {
-                pointPerResult = question.points
+            if (currentQuestion.right_answers.every(v => currentQuestionResult.user_answers.indexOf(v) >= 0)) {
+                pointPerResult = currentQuestion.points
             } else pointPerResult = 0
         }
 
@@ -100,18 +100,31 @@ exports.addQuestionResults = async (req, reply) => {
 
         let nextQuestion = "-1";
 
-        allUserQuestionResults = allUserQuestionResults.map(s => s.question)
+        allUserQuestionResults = await Promise.all(allUserQuestionResults.map(async (s) => s.question))
         if (currentTest.medium_questions.length === allUserQuestionResults.length) {
             nextQuestion = {}
             return reply.code(200).send(nextQuestion)
         }
 
         const fuzzyResult = FuzzyController.getNextQuestionLevel(pointPerResult / maxPointPerQuestion, currentTestPoints / maxPointPerTest)
+
+        console.log(pointPerResult)
+        console.log(maxPointPerQuestion)
+        console.log(currentTestPoints / maxPointPerTest)
+        console.log(fuzzyResult)
+
+        console.log(allUserQuestionResults)
+
         switch (fuzzyResult) {
             case -1:
-                allQuestionsPerTest = await Promise.all(allQuestionsPerTest.easy_questions.filter(async (s) => allUserQuestionResults.includes(s)))
-                if (allQuestionsPerTest.length > 0) {
-                    nextQuestion = await Question.findById(allQuestionsPerTest[0])
+                allQuestionsPerTest.easy_questions = await Promise.all(allQuestionsPerTest.easy_questions.filter(async (s) => {
+                    console.log(s)
+                    console.log(allUserQuestionResults.includes(s))
+                    return !allUserQuestionResults.includes(s)
+                }))
+
+                if (allQuestionsPerTest.easy_questions.length > 0) {
+                    nextQuestion = await Question.findById(allQuestionsPerTest.easy_questions[0])
 
                     if (nextQuestion != null) {
                         nextQuestion.right_answers = undefined
@@ -120,9 +133,13 @@ exports.addQuestionResults = async (req, reply) => {
                     }
                 }
             case 0:
-                allQuestionsPerTest = await Promise.all(allQuestionsPerTest.medium_questions.filter(async (s) => allUserQuestionResults.includes(s)))
-                if (allQuestionsPerTest.length > 0) {
-                    nextQuestion = await Question.findById(allQuestionsPerTest[0])
+                allQuestionsPerTest.medium_questions = await Promise.all(allQuestionsPerTest.medium_questions.filter(async (s) => {
+                    console.log(s)
+                    console.log(allUserQuestionResults.includes(s))
+                    return !allUserQuestionResults.includes(s)
+                }))
+                if (allQuestionsPerTest.medium_questions.length > 0) {
+                    nextQuestion = await Question.findById(allQuestionsPerTest.medium_questions[0])
 
                     if (nextQuestion != null) {
                         nextQuestion.right_answers = undefined
@@ -130,9 +147,13 @@ exports.addQuestionResults = async (req, reply) => {
                     }
                 }
             case 1:
-                allQuestionsPerTest = await Promise.all(allQuestionsPerTest.difficult_questions.filter(async (s) => allUserQuestionResults.includes(s)))
-                if (allQuestionsPerTest.length > 0) {
-                    nextQuestion = await Question.findById(allQuestionsPerTest[0])
+                allQuestionsPerTest.difficult_questions = await Promise.all(allQuestionsPerTest.difficult_questions.filter(async (s) => {
+                    console.log(s)
+                    console.log(allUserQuestionResults.includes(s))
+                    return !allUserQuestionResults.includes(s)
+                }))
+                if (allQuestionsPerTest.difficult_questions.length > 0) {
+                    nextQuestion = await Question.findById(allQuestionsPerTest.difficult_questions[0])
 
                     if (nextQuestion != null) {
                         nextQuestion.right_answers = undefined
